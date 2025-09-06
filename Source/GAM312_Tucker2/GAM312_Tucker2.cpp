ff42b7415a9c7373fe1473138217e1d8ed9e8eaa
Catarena Tucker <catarena.tucker@snhu.edu>
2025-09-06 14:55:05 -0500

    Initial Commit

---
+++ Source/GAM312_Tucker2/GAM312_Tucker2.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GAM312_Tucker2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GAM312_Tucker2, "GAM312_Tucker2" );
